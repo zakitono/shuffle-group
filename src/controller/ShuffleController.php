5c2c95ff68ac24faa00b5e88a0af6ed4f3f8969e
zakitono <coppercraft1313@gmail.com>
2023-05-06 15:53:11 +0900

    ShuffleControllerへ追記、index.phpの削除

--- src/controller/ShuffleController.php
+++ src/controller/ShuffleController.php
@@ -1,13 +1,42 @@
 <?php
-class ShuffleController
+
+require_once __DIR__ . '/../core/Controller.php';
+
+class ShuffleController extends Controller
 {
-    public function run($action)
+    public function index()
     {
-        $this->$action();
+        $groups = [];
+
+        $mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
+        if ($mysqli->connect_error) {
+            throw new RuntimeException('mysqli接続エラー:' . $mysqli->connect_error);
+        }
+
+        include  __DIR__ . '/../views/index.php';
     }
 
-    private function index()
+    public function create()
     {
-        echo 'Hello, index';
+        $groups = [];
+        $mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
+        if ($mysqli->connect_error) {
+            throw new RuntimeException('mysqli接続エラー:' . $mysqli->connect_error);
+        }
+
+        $result = $mysqli->query('SELECT name FROM employees');
+        $employees = $result->fetch_all(MYSQLI_ASSOC);
+        shuffle($employees);
+        $cnt = count($employees);
+
+        if ($cnt % 2 === 0) {
+            $groups = array_chunk($employees, 2);
+        } else {
+            $extra = array_pop($employees);
+            $groups = array_chunk($employees, 2);
+            array_push($groups[0], $extra);
+        }
+
+        include  __DIR__ . '/../views/index.php';
     }
 }
