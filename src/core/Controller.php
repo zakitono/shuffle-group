6902e4a922f1fc6dcb3bb5d2c58992f941230019
zakitono <coppercraft1313@gmail.com>
2023-05-08 13:24:16 +0900

    renderメソッドの追加 viewファイルをcontrollerから分離

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -2,11 +2,32 @@
 
 class Controller
 {
+    protected $actionName;
+
     public function run($action)
     {
+        $this->actionName = $action;
+
         if (!method_exists($this, $action)) {
             throw new HttpNotFoundEx();
         }
-        $this->$action();
+        $content = $this->$action();
+        return $content;
+    }
+
+    //$template → アクション名
+    protected function render($variables = [], $template = null, $layout = 'layout')
+    {
+        $view = new View(__DIR__ . '/../views');
+
+        if (is_null($template)) {
+            $template = $this->actionName;
+        }
+        //クラス名の前部分を取得
+        $controllerName = strtolower(substr(get_class($this), 0, -10));
+        // shuffle, employee
+        $path = $controllerName . '/' . $template;
+        // shuffle/index
+        return $view->render($path, $variables, $layout);
     }
 }
