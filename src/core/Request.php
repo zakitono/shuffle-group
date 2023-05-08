f68bed1f51f39d6d1d08699e042d16bb614fa942
zakitono <coppercraft1313@gmail.com>
2023-05-08 15:14:20 +0900

    Requestクラスの作成、GETリクエスト時の処理

---
+++ src/core/Request.php
@@ -0,0 +1,17 @@
+<?php
+
+class Request
+{
+    public function isPost()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            return true;
+        }
+        return false;
+    }
+
+    public function getPathInfo()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+}
