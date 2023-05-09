13a7ceb84c45a44e94786d2de7601034e9c5da5a
zakitono <coppercraft1313@gmail.com>
2023-05-09 23:12:04 +0900

    社員登録時に、リロードの文言が出るように修正

--- src/controller/EmployeeController.php
+++ src/controller/EmployeeController.php
@@ -21,6 +21,7 @@ class EmployeeController extends Controller
         }
 
         $errors = [];
+        $register = [];
 
         $employee = $this->databaseManager->get('Employee');
         $employees = $employee->fetchAllNames();
@@ -35,43 +36,14 @@ class EmployeeController extends Controller
         }
         if (!count($errors)) {
             $employee->insert($_POST['name']);
+            $register = "登録が完了しました。1度リロードしてください。";
         }
 
         return $this->render([
             'title' => '社員の登録',
             'employees' => $employees,
             'errors' => $errors,
-        ], 'index');
-    }
-
-    public function validationCreate()
-    {
-
-        if (!$this->request->isPost()) {
-            throw new HttpNotFoundEx();
-        }
-
-        $errors = [];
-
-        $employee = $this->databaseManager->get('Employee');
-        $employees = $employee->fetchAllNames();
-
-        #バリデーション
-        if (!strlen($_POST['name'])) {
-            if (!strlen($_POST['name'])) {
-                $errors['name'] = '社員名を入力してください。';
-            } elseif (strlen($_POST['name']) > 100) {
-                $errors['name'] = '社員名は100文字以内で入力してください。';
-            }
-        }
-        if (!count($errors)) {
-            $employee->insert($_POST['name']);
-        }
-
-        return $this->render([
-            'title' => '社員の登録',
-            'employees' => $employees,
-            'errors' => $errors,
-        ], 'index');
+            'register' => $register,
+        ], 'create');
     }
 }
