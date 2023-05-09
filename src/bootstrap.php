d9926f336e3bb4180ac2242e132cf6025ad75320
zakitono <coppercraft1313@gmail.com>
2023-05-09 13:12:57 +0900

    modelsディレクトリをAutoLoder.phpへ追記

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,4 +5,5 @@ require 'core/AutoLoader.php';
 $loader = new AutoLoader();
 $loader->registerDir(__DIR__ . '/core');
 $loader->registerDir(__DIR__ . '/controller');
+$loader->registerDir(__DIR__ . '/models');
 $loader->register();
