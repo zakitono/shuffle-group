af2f58c5c163b279f1fe348bd9bd296f11c5755f
zakitono <coppercraft1313@gmail.com>
2023-05-09 18:27:46 +0900

    validationCreateアクションの追加

--- src/controller/EmployeeController.php
+++ src/controller/EmployeeController.php
@@ -43,4 +43,35 @@ class EmployeeController extends Controller
             'errors' => $errors,
         ], 'index');
     }
+
+    public function validationCreate()
+    {
+
+        if (!$this->request->isPost()) {
+            throw new HttpNotFoundEx();
+        }
+
+        $errors = [];
+
+        $employee = $this->databaseManager->get('Employee');
+        $employees = $employee->fetchAllNames();
+
+        #バリデーション
+        if (!strlen($_POST['name'])) {
+            if (!strlen($_POST['name'])) {
+                $errors['name'] = '社員名を入力してください。';
+            } elseif (strlen($_POST['name']) > 100) {
+                $errors['name'] = '社員名は100文字以内で入力してください。';
+            }
+        }
+        if (!count($errors)) {
+            $employee->insert($_POST['name']);
+        }
+
+        return $this->render([
+            'title' => '社員の登録',
+            'employees' => $employees,
+            'errors' => $errors,
+        ], 'index');
+    }
 }
