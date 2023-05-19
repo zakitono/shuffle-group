ee2867905b95beb8884e84f828da61bc970e0757
zakitono <coppercraft1313@gmail.com>
2023-05-19 17:51:14 +0900

    コメントの追加

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,8 +1,10 @@
+<!-- 初期化の処理 -->
 <?php
 
 require 'core/AutoLoader.php';
 
 $loader = new AutoLoader();
+//ディレクトリの登録
 $loader->registerDir(__DIR__ . '/core');
 $loader->registerDir(__DIR__ . '/controller');
 $loader->registerDir(__DIR__ . '/models');
